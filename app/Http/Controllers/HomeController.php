44e72b82cdc5acba9a006302d2de46c74a00af4a
ahmad reza jalali <hamed.classic.2te@gmail.com>
2021-01-27 12:18:50 +0330

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller {
      */
     public function __construct() 
     {
-         $this->middleware('check.info.user');
+        // $this->middleware('check.info.user');
     }
 
     /**
